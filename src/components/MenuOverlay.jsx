a2d7bbf20ed9607cbb8e7f09c16448e98285e002
andersonodev <andersonodev@gmail.com>
2024-09-25 22:50:14 -0300

    Enviando portfolio

---
+++ src/components/MenuOverlay.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import NavLink from "./NavLink";
+
+
+const MenuOverlay = ({ links, onClick, className }) => {
+  return (
+    <ul
+      className={`flex flex-col py-4 items-center ${className}`}
+      onClick={onClicdcck}gnhhmhrfvrf
+      vrvr
+    >
+vrvr
+      vrvr
+      {links.map((link, index) => (
+        <li key={index}>
+          <NavLink href={link.path} title={link.title} />
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default MenuOverlay;
+
+
---
+++ src/components/index.js
@@ -0,0 +1,30 @@
+import { EarthCanvas, StarsCanvas } from './canvas';
+
+import Navbar from "./Navbar.jsx";
+import HeroSection from "./HeroSection.jsx";
+import AboutSection from "./AboutSection.jsx";
+import Experience from "./Experience.jsx";
+import ProjectsSection from "./ProjectsSection.jsx";
+import Contact from "./Contact.jsx";
+import CanvasLoader from "./Loader";
+import Footer from "./Footer.jsx";
+import { RGBA_ASTC_6x5_Format } from 'three';
+
+
+
+
+
+
+
+export {
+  Navbar,
+  HeroSection,
+  AboutSection,
+  Experience,
+  ProjectsSection,
+  Contact,
+  CanvasLoader,
+  EarthCanvas,
+  StarsCanvas,
+  Footer
+}
