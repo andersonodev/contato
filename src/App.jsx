3e28aa1f87efac951ffdddeaf0ccdff3edf5de98
andersonodev <andersonodev@gmail.com>
2025-02-24 12:34:54 -0300

    upload

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,6 @@ import { BrowserRouter } from "react-router-dom";
 import {
   Navbar,
   // HeroSection,
-  // AboutSection,
-  // Experience,
-  // ProjectsSection,
   Contact,
   StarsCanvas,
   Footer,
@@ -23,18 +20,6 @@ const App = () => {
         {/* <div className="container mt-24 mx-auto px-12 py-4">
           {/* Seção principal */}
           {/* <HeroSection /> */}
-
-          {/* Seção comentada | pausada */}
-          {/* <AchievementsSection /> */}
-
-          {/* Seção sobre */}
-          {/* <AboutSection /> */}
-
-          {/* <Experience/> */}
-
-          {/* Seção de projetos */}
-          {/* <ProjectsSection /> */}
-        {/* </div> */} */}
 
         {/* Seção de contato e canvas de estrelas/planeta */}
         <div className="relative z-0 bg-primaryOne">
--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,11 @@
 import { EarthCanvas, StarsCanvas } from './canvas';
 
 import Navbar from "./Navbar.jsx";
-import HeroSection from "./HeroSection.jsx";
-import AboutSection from "./AboutSection.jsx";
-import Experience from "./Experience.jsx";
-import ProjectsSection from "./ProjectsSection.jsx";
 import Contact from "./Contact.jsx";
 import CanvasLoader from "./Loader";
 import Footer from "./Footer.jsx";
-import { RGBA_ASTC_6x5_Format } from 'three';
+// import Hero from "./HeroSection.jsx";
+
 
 
 
@@ -17,11 +14,8 @@ import { RGBA_ASTC_6x5_Format } from 'three';
 
 
 export {
+  // Hero,
   Navbar,
-  HeroSection,
-  AboutSection,
-  Experience,
-  ProjectsSection,
   Contact,
   CanvasLoader,
   EarthCanvas,
